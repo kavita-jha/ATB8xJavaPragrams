35ce40d3670ee58473cdd92d2b8b8ae47205cf75
kavita_jha <kavita100219@gmail.com>
2024-09-14 17:15:09 +0530

    Add files

---
+++ src/sept/ex_06092024/Lab004.java
@@ -0,0 +1,16 @@
+package sept.ex_06092024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+
+
+
+
+    }
+}
+
