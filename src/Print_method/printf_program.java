ba6b46fa09a2d71c9dc39e783eae1d27f4ec1df4
kavita_jha <kavita100219@gmail.com>
2024-10-01 16:47:30 +0530

    Add files

---
+++ src/Print_method/printf_program.java
@@ -0,0 +1,23 @@
+package Print_method;
+
+public class printf_program {
+    public static void main(String[] args) {
+        int num=21;
+        System.out.printf("Value of a %d",num);
+        System.out.println();
+        short sh=1;
+        System.out.printf("value of short %d",sh);
+        System.out.println();
+        long l=1234456l;
+        System.out.printf("Value of long %d",l);
+        System.out.println();
+        float f= 34.43f;
+        System.out.printf("Value of float %f",f);
+        System.out.println();
+        String s="Hello everyone, greetings ti all!";
+        System.out.printf("Value of string is %s",s);
+        System.out.println();
+        char c='K';
+        System.out.printf("Value of char is %c",c);
+    }
+}
